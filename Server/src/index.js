89dcd77a4acfb72dedc190e008fd6dfd12641ec0
Adhityak03 <adhityaadhi10@gmail.com>
2026-06-23 19:38:38 +0530

    day 2 schema setup is done for users

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const User = require("./models/User");
+
 
 // Load env variables
 dotenv.config();
@@ -22,6 +24,23 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
+
+    const createUser=async () => {
+        try{
+            const user =await User.create(
+                {
+                    name: "Adhitya",
+                    email: "adhitya@test.com",
+                    password: "123456"
+                }
+            )
+            console.log("User created:", user);
+        }
+        catch (error) {
+        console.log(error.message);
+    }
+    }
+
 
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
