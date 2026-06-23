f0e335a6498501d294459389a568556fcfa1806c
Adhityak03 <adhityaadhi10@gmail.com>
2026-06-23 16:08:32 +0530

    Day 1 backend setup completed

---
+++ Server/src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// Load env variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Job Tracker API running...");
+});
+
+// Connect database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
